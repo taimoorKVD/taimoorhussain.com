2391a0151de6e1c76cb439b4acfd93c2c576bc09
Maaz-KV <maaz1.kingdomvision@gmail.com>
2025-04-06 15:49:12 +0500

    Create Api

--- app/Http/Controllers/Website/PageController.php
+++ app/Http/Controllers/Website/PageController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
-use App\Models\About;
-use App\Models\Home;
-use App\Models\Portfolio;
-use App\Models\Service;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
+use App\Models\{About, Home, Portfolio, Service};
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function __invoke(Request $request): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    public function __invoke(Request $request): \Illuminate\Http\JsonResponse
     {
-        return view('website.index')
-            ->with('home', Home::select('id', 'title', 'description', 'image', 'social_link')->first()->toArray())
-            ->with('about', About::select('title', 'description', 'image', 'skill', 'email', 'contact', 'nationality', 'address')->first()->toArray())
-            ->with('services', Service::select('id','title', 'description', 'image', 'detail')->get()->toArray())
-            ->with('portfolios', Portfolio::select('id', 'title', 'description', 'category', 'stacks', 'client', 'date', 'url', 'image')->get()->toArray());
+        $domain_name = $request->getHost();
+
+        return response()->json([
+            'home' => Home::select('id', 'title', 'description', 'image', 'social_link')->where('domain_name' , $domain_name)->first()->toArray(),
+            'about' => About::select('title', 'description', 'image', 'skill', 'email', 'contact', 'nationality', 'address')->where('domain_name' , $domain_name)->first()->toArray(),
+            'services' => Service::select('id','title', 'description', 'image', 'detail')->whereJsonContains('domain_name' , $domain_name)->get()->toArray(),
+            'portfolios' => Portfolio::select('id', 'title', 'description', 'category', 'stacks', 'client', 'date', 'url', 'image')->whereJsonContains('domain_name' , $domain_name)->get()->toArray()
+        ]);
+
+//        return view('website.index')
+//            ->with('home', Home::select('id', 'title', 'description', 'image', 'social_link')->first()->toArray())
+//            ->with('about', About::select('title', 'description', 'image', 'skill', 'email', 'contact', 'nationality', 'address')->first()->toArray())
+//            ->with('services', Service::select('id','title', 'description', 'image', 'detail')->get()->toArray())
+//            ->with('portfolios', Portfolio::select('id', 'title', 'description', 'category', 'stacks', 'client', 'date', 'url', 'image')->get()->toArray());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Website\{ContactController, PageController};
+use App\Http\Middleware\LogVisitor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/', PageController::class)->middleware(LogVisitor::class);
+Route::post('new-contact', ContactController::class)->name('contact.store');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
